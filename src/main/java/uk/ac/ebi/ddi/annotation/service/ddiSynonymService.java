a637280f20ff3530bfc502a85985b1cd67a3b5ca
baimingze <baimingze@gmail.com>
2015-07-27 10:01:34 +0100

    first commit

---
+++ src/main/java/uk/ac/ebi/ddi/annotation/service/ddiSynonymService.java
@@ -0,0 +1,55 @@
+package uk.ac.ebi.ddi.annotation.service;
+
+import com.mongodb.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import uk.ac.ebi.ddi.annotation.service.ddiAnnotationService;
+/**
+ * Created by mingze on 22/07/15.
+ *
+ * find  synonym list for a word, if doesn't have, create one and stored in MongoDB
+ */
+public class ddiSynonymService {
+
+    public List<String> getSynonyms(String word) throws IOException {
+
+        MongoClient mongo = new MongoClient("localhost", 27017);
+        DB db = mongo.getDB("SynonymDB");
+        DBCollection table = db.getCollection("synonyms");
+
+        ArrayList<String>  synonyms =  getSynonymsInList(word,table);
+
+        if (synonyms.size()==0) {
+            ddiAnnotationService annotService = new ddiAnnotationService();
+            synonyms =  annotService.getSynonymsForWord(word);
+            insertInSynonymsList(synonyms, table);
+        }
+//        BasicDBObject document = new BasicDBObject();
+//        document.put("_id", "test");
+//        document.put("next_id", "test2");
+//        table.insert(document);
+        return synonyms;
+    }
+
+    private void insertInSynonymsList(ArrayList<String> synonyms, DBCollection table) {
+
+
+    }
+
+    private ArrayList<String> getSynonymsInList(String word, DBCollection table) {
+        List<String>  synonyms = new ArrayList<>();
+        BasicDBObject searchQuery = new BasicDBObject();
+        searchQuery.put("_id", word);
+        DBCursor cursor = table.find(searchQuery);
+
+        while (cursor.hasNext()) {
+            System.out.println(cursor.next());
+        }
+
+        return null;
+    }
+
+}
