48181a4d700e34a825e35c4e7061715faf52e654
¨gaurhari¨ <gaurharidass@gmail.com>
2017-08-22 17:10:25 +0100

    updated multiomics value of constants

--- src/main/java/uk/ac/ebi/ddi/annotation/utils/Constants.java
+++ src/main/java/uk/ac/ebi/ddi/annotation/utils/Constants.java
@@ -34,5 +34,73 @@ public class Constants {
     public static final String CLASSES = "/classes/";
     public static final String ENSEMBL_DATABASE = "ensembl";
     public static final String UNIPROT_DATABASE = "uniprot";
-    public static final String MULTIOMICS_TYPE  = "Multi-Omics";
+    public static final String MULTIOMICS_TYPE  = "Multiomics";
+    public static final String TAXONOMY_FIELD           = "TAXONOMY";
+
+    public static final String MAIN_DOMAIN              = "omics";
+
+    public static final String REPOSITORY_TAG           = "Repositories";
+
+    public static final String TISSUE_FIELD             = "tissue" ;
+
+    public static final String OMICS_TYPE_FIELD         = "omics_type";
+
+    public static final String DISEASE_FIELD            = "disease";
+
+    public static final String DESCRIPTION_FIELD        = "description";
+
+    public static final String NAME_FIELD               = "name";
+
+    public static final String SUBMITTER_KEY_FIELD      = "submitter_keywords";
+
+    public static final String CURATOR_KEY_FIELD        = "curator_keywords";
+
+    public static final String PUB_DATE_FIELD           = "publication_date";
+
+    public static final String PUB_DATE_FIELD_OPTIONAL  = "publication";
+
+    public static final String[] PUB_DATES = new String[]{PUB_DATE_FIELD, PUB_DATE_FIELD_OPTIONAL};
+
+    public static final String EGA_UPDATED_FIELD        = "updated";
+
+    public static final String DATA_PROTOCOL_FIELD      = "data_protocol";
+
+    public static final String SAMPLE_PROTOCOL_FIELD    = "sample_protocol";
+
+    public static final String PUBMED_FIELD             = "pubmed";
+
+    public static final String DATASET_LINK_FIELD       = "full_dataset_link";
+
+    public static final String INSTRUMENT_FIELD         = "instrument_platform";
+
+    public static final String EXPERIMENT_TYPE_FIELD    = "technology_type";
+
+    public static final String ORGANIZATION_FIELD       = "submitter_affiliation";
+
+    public static final String DATES_FIELD              = "dates";
+
+    public static String SUBMITTER_FIELD                = "submitter";
+
+    public static String SUBMITTER_MAIL_FIELD           = "submitter_mail";
+
+    public static String LAB_HEAD_FIELD                 =  "labhead";
+
+    public static String LAB_HEAD_MAIL_FIELD            =  "labhead_mail";
+
+    public static String ENSEMBL                        =   "ENSEMBL";
+
+    public static String UNIPROT                        =   "UNIPROT";
+
+    public static String CHEBI                          =   "CHEBI";
+
+    public static final String[] DATASET_SUMMARY        = {Constants.DESCRIPTION_FIELD,
+            Constants.NAME_FIELD,
+            Constants.SUBMITTER_KEY_FIELD,Constants.CURATOR_KEY_FIELD,
+            Constants.PUB_DATE_FIELD,
+            Constants.TAXONOMY_FIELD,
+            Constants.OMICS_TYPE_FIELD,
+            Constants. ENSEMBL,
+            Constants.UNIPROT,
+            Constants.CHEBI};
+
 }
