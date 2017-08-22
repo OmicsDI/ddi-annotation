b6d4d67f906382a2ac8d0d534dda8bf85d35045a
¨gaurhari¨ <gaurharidass@gmail.com>
2017-08-22 17:12:25 +0100

    test cases for simalrity

---
+++ src/test/java/uk/ac/ebi/ddi/similaritycalculator/SimilarityCountTest.java
@@ -0,0 +1,28 @@
+package uk.ac.ebi.ddi.similaritycalculator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import uk.ac.ebi.ddi.similarityCalculator.SimilarityCounts;
+
+/**
+ * Created by gaur on 13/07/17.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/applicationTestContext.xml"})
+public class SimilarityCountTest {
+
+    @Autowired
+    SimilarityCounts similarityCounts;
+
+    @Test
+    public void getCitationCount(){
+        similarityCounts.getCitationCount("ArrayExpress","E-MEXP-981");
+    }
+
+    
+}
+
+
