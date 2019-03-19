2535116b7fbd4dedd74a100f5439d6b239aa8607
Gaurhari <gaurharidass@gmail.com>
2019-03-19 10:33:40 +0000

    similarity related constants

---
+++ src/main/java/uk/ac/ebi/ddi/similarityCalculator/utils/SimilarityConstants.java
@@ -0,0 +1,12 @@
+package uk.ac.ebi.ddi.similarityCalculator.utils;
+
+/*
+* @author Gaurhari Dass gdass@ebi.ac.uk
+* */
+public class SimilarityConstants {
+
+    public static final String OMICS_DOMAIN = "omics";
+
+
+
+}
