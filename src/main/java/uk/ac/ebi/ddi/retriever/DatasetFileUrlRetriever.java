e84a72e0e197627302763778bba9ba69d79086e8
Tu Manh Vu <glmanhtu@gmail.com>
2019-04-03 14:46:45 +0100

    Ignore ssl verification

--- src/main/java/uk/ac/ebi/ddi/retriever/DatasetFileUrlRetriever.java
+++ src/main/java/uk/ac/ebi/ddi/retriever/DatasetFileUrlRetriever.java
@@ -3,6 +3,8 @@ package uk.ac.ebi.ddi.retriever;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.CookieSpecs;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
 import org.apache.http.impl.client.HttpClients;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpRequestFactory;
@@ -11,7 +13,12 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 import org.springframework.web.client.RestTemplate;
 import uk.ac.ebi.ddi.extservices.utils.RetryClient;
 
+import javax.net.ssl.SSLContext;
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.util.Collections;
 import java.util.Set;
 
@@ -23,7 +30,11 @@ public abstract class DatasetFileUrlRetriever extends RetryClient implements IDa
 
     public DatasetFileUrlRetriever(IDatasetFileUrlRetriever datasetDownloadingRetriever) {
         this.datasetDownloadingRetriever = datasetDownloadingRetriever;
-        this.restTemplate = new RestTemplate(clientHttpRequestFactory());
+        try {
+            this.restTemplate = new RestTemplate(clientHttpRequestFactory());
+        } catch (KeyStoreException | NoSuchAlgorithmException | KeyManagementException e) {
+            throw new RuntimeException(e);
+        }
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
         converter.setSupportedMediaTypes(Collections.singletonList(MediaType.TEXT_HTML));
         restTemplate.getMessageConverters().add(converter);
@@ -38,11 +49,20 @@ public abstract class DatasetFileUrlRetriever extends RetryClient implements IDa
         return result;
     }
 
-    private ClientHttpRequestFactory clientHttpRequestFactory() {
+    private ClientHttpRequestFactory clientHttpRequestFactory()
+            throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
         HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+
+        SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
+                .loadTrustMaterial(null, acceptingTrustStrategy)
+                .build();
+
+        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
         factory.setReadTimeout(200000);
         factory.setConnectTimeout(200000);
         HttpClient httpClient = HttpClients.custom()
+                .setSSLSocketFactory(csf)
                 .setDefaultRequestConfig(RequestConfig.custom()
                         .setCookieSpec(CookieSpecs.STANDARD).build())
                 .build();
